c03fafb9003ea4e4d1be26fd8bf88822b702dffa
kvIT-389 <64485615+kvIT-389@users.noreply.github.com>
2022-12-09 21:56:24 +0800

    Add methods for select and insert data

--- RequsetHandler/DBTypes.py
+++ RequsetHandler/DBTypes.py
@@ -1,26 +1,32 @@
 import datetime
 from dataclasses import dataclass
+from abc import ABC
 
 
 @dataclass(frozen=True)
-class Attend:
+class DBObjectBase(ABC):
+    pass
+
+@dataclass(frozen=True)
+class Attend(DBObjectBase):
     student_id: int
     class_number: int
-    attend: bool
+    attend_time: datetime.datetime
 
 
 @dataclass(frozen=True)
-class Student:
+class Student(DBObjectBase):
     student_id: int
+    group_id: int
     last_name: str
     first_name: str
     middle_name: str
-    group_id: int
-    face: dict[str]
+    face: bytes
 
 
 @dataclass(frozen=True)
-class Schedule:
+class Schedule(DBObjectBase):
     class_number: int
     begin_time: datetime.datetime
     end_time: datetime.datetime
+    group_id: int
--- RequsetHandler/request_handler.py
+++ RequsetHandler/request_handler.py
@@ -1,7 +1,8 @@
 import sqlite3
 from dataclasses import asdict
+from typing import Any
 
-from DBTypes import Student, Attend, Schedule
+from RequsetHandler.DBTypes import DBObjectBase
 
 
 class RequestHandler:
@@ -15,28 +16,129 @@ class RequestHandler:
     def __connect_db(self, db_path: str) -> None:
         try:
             self.connection = sqlite3.connect(db_path)
+
         except sqlite3.ProgrammingError:
             # add logging
             pass
 
-    def insert_data(self, table_name: str, data: list):
+        finally:
+            return
+
+
+    def insert_one(self, table_name: str, obj: DBObjectBase, *fields: str):
+        """
+        Insert one new record given from `obj` to the `table_name`.
+        If `fields` is not empty, insert only given object fields.
+        """
+
         cursor = self.connection.cursor()
 
         try:
-            for cur_data in data:
-                dict_data = asdict(cur_data)
-                cursor.execute(f'INSERT INTO {table_name}({", ".join(dict_data.keys())}) VALUES({", ".join("?" for _ in range(len(dict_data)))})', cur_data)
+            obj_dict: dict[str] = asdict(obj)
+            f: tuple[str] = fields if len(fields) else obj_dict.keys()
+
+            data_tuple: tuple = tuple(obj_dict.get(field) for field in f)
+
+            query: str = "INSERT INTO {}({}) VALUES({})".format(
+                table_name,
+                ','.join(f),
+                ','.join(['?'] * len(f))
+            )
+            cursor.execute(query, data_tuple)
+
         except sqlite3.ProgrammingError:
             # add logging
             pass
 
-        cursor.close()
+        finally:
+            self.connection.commit()
+            cursor.close()
 
-    def update_db(self):
+    def insert_many(self, table_name: str, objs: list[DBObjectBase], *fields: str):
+        """
+        Insert records given from `objs` to the `table_name`.
+        If `fields` is not empty, insert only given object fields.
+        """
+
+        cursor = self.connection.cursor()
+
+        try:
+            f: tuple[str] = fields if len(fields) else asdict(objs[0]).keys()
+
+            data_tuples: list[tuple] = []
+            for obj in objs:
+                obj_dict: dict[str] = asdict(obj)
+                data_tuples.append(tuple(obj_dict.get(field) for field in f))
+
+            query: str = "INSERT INTO {}({}) VALUES({})".format(
+                table_name,
+                ','.join(f),
+                ','.join(['?'] * len(f))
+            )
+            cursor.executemany(query, data_tuples)
+
+        except sqlite3.ProgrammingError:
+            # add logging
+            pass
+
+        finally:
+            self.connection.commit()
+            cursor.close()
+
+
+    def update(self, table_name: str, fields: list[str], data: list[DBObjectBase]):
         pass
 
-    def get_data(self):
-        pass
+
+    def select_one(self, table_name: str, condition: str, *fields: str) -> Any:
+        """
+        Selects one record from `table_name` satisfying the `condition`.
+        If `fields` is not empty, select only given table fields.
+        """
+
+        cursor = self.connection.cursor()
+
+        try:
+            query: str = "SELECT {} FROM {} WHERE {}".format(
+                ','.join(fields) if len(fields) else '*',
+                table_name,
+                condition
+            )
+            cursor.execute(query)
+
+            data: Any = cursor.fetchone()
+
+        except sqlite3.ProgrammingError:
+            # add logging
+            pass
+
+        finally:
+            return data
+
+    def select_all(self, table_name: str, condition: str, *fields: str) -> list[Any]:
+        """
+        Selects all records from `table_name` satisfying the `condition`.
+        If `fields` is not empty, select only given table fields.
+        """
+
+        cursor = self.connection.cursor()
+
+        try:
+            query: str = "SELECT {} FROM {} WHERE {}".format(
+                ','.join(fields) if len(fields) else '*',
+                table_name,
+                condition
+            )
+            cursor.execute(query)
+
+            data: list[Any] = cursor.fetchall()
+
+        except sqlite3.ProgrammingError:
+            # add logging
+            pass
+
+        finally:
+            return data
 
     def is_connected(self) -> bool:
         return self.connection is not None
