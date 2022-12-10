c166e4e9656ec99952e954742d73c63546637ced
kvIT-389 <64485615+kvIT-389@users.noreply.github.com>
2022-12-10 14:28:46 +0800

    Refactor RequestHandler class and fix insert_many method

--- RequestHandler/request_handler.py
+++ RequestHandler/request_handler.py
@@ -24,40 +24,30 @@ class RequestHandler:
         finally:
             return
 
-    def insert_one(self, table_name: str, obj: DBObjectBase, *fields: str):
+    def is_connected(self) -> bool:
+        return self.connection is not None
+
+    def insert_one(self, table_name: str,
+                   obj: DBObjectBase,
+                   *fields: str) -> None:
         """
         Insert one new record given from `obj` to the `table_name`.
         If `fields` is not empty, insert only given object fields.
         """
 
-        cursor: sqlite3.Cursor = self.connection.cursor()
+        self.insert_many(table_name, [obj], *fields)
 
-        try:
-            obj_dict: dict[str] = asdict(obj)
-            f: tuple[str] = fields if len(fields) else obj_dict.keys()
-
-            data_tuple: tuple = tuple(obj_dict.get(field) for field in f)
-
-            query: str = "INSERT INTO {}({}) VALUES({})".format(
-                table_name,
-                ','.join(f),
-                ','.join(['?'] * len(f))
-            )
-            cursor.execute(query, data_tuple)
-
-        except sqlite3.ProgrammingError:
-            # add logging
-            pass
-
-        finally:
-            self.connection.commit()
-            cursor.close()
-
-    def insert_many(self, table_name: str, objs: list[DBObjectBase], *fields: str):
+    def insert_many(self, table_name: str,
+                    objs: list[DBObjectBase],
+                    *fields: str) -> None:
         """
         Insert records given from `objs` to the `table_name`.
         If `fields` is not empty, insert only given object fields.
+        If `objs` is empty list, nothing happens.
         """
+
+        if (len(objs) == 0):
+            return
 
         cursor: sqlite3.Cursor = self.connection.cursor()
 
@@ -85,9 +75,14 @@ class RequestHandler:
             cursor.close()
 
     def update(self, table_name: str,
-                            data: dict[str, Any],
-                            condition: str | None = None):
-        if len(data) == 0:
+               data: dict[str, Any],
+               condition: str | None = None):
+        """
+        Update records in `table_name` satisfying `condition`.
+        If `data` is empty dict, nothing happens.
+        """
+
+        if (len(data) == 0):
             return
 
         cursor: sqlite3.Cursor = self.connection.cursor()
@@ -100,7 +95,7 @@ class RequestHandler:
             )
             cursor.execute(query)
 
-        except Exception as e:
+        except sqlite3.ProgrammingError:
             # add logging
             pass
 
@@ -108,32 +103,21 @@ class RequestHandler:
             self.connection.commit()
             cursor.close()
 
-    def select_one(self, table_name: str, condition: str | None = None, *fields: str) -> Any:
+    def select_one(self, table_name: str,
+                   condition: str | None = None,
+                   *fields: str) -> Any:
         """
-        Selects one record from `table_name` satisfying the `condition`.
+        Selects first record from `table_name` satisfying the `condition`.
         If `fields` is not empty, select only given table fields.
         """
 
-        cursor: sqlite3.Cursor = self.connection.cursor()
+        r = self.select_all(table_name, condition, *fields)[0]
 
-        try:
-            query: str = "SELECT {} FROM {} {}".format(
-                ','.join(fields) if len(fields) else '*',
-                table_name,
-                f"WHERE {condition}" if condition else ""
-            )
-            cursor.execute(query)
+        return r[0] if len(r) else None
 
-            data: Any = cursor.fetchone()
-
-        except sqlite3.ProgrammingError:
-            # add logging
-            pass
-
-        finally:
-            return data
-
-    def select_all(self, table_name: str, condition: str | None = None, *fields: str) -> list[Any]:
+    def select_all(self, table_name: str,
+                   condition: str | None = None,
+                   *fields: str) -> list[Any]:
         """
         Selects all records from `table_name` satisfying the `condition`.
         If `fields` is not empty, select only given table fields.
@@ -158,7 +142,8 @@ class RequestHandler:
         finally:
             return data
 
-    def delete_all(self, table_name: str, condition: str | None = None) -> None:
+    def delete_all(self, table_name: str,
+                   condition: str | None = None) -> None:
         """
         Delete all records from `table_name` satisfying the `condition`.
         If `condition` is `None` clear `table_name`.
@@ -180,6 +165,3 @@ class RequestHandler:
         finally:
             self.connection.commit()
             cursor.close()
-
-    def is_connected(self) -> bool:
-        return self.connection is not None
