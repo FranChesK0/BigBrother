325dec9b5facec07279930aedea416978936a3f0
FranChesKo <khl_doss@mail.ru>
2022-12-10 15:27:46 +0800

    Change FaceEncoding from multiple images converting to one image

--- EncodingHandler/encoding_handler.py
+++ EncodingHandler/encoding_handler.py
@@ -6,17 +6,15 @@ import face_recognition
 
 class FaceEncoding:
     @staticmethod
-    def get_face_encoding(*image_paths: str) -> bytes:
+    def get_face_encoding(image_path: str) -> bytes:
         """
         Method for getting embedding of faces from images
 
-        :param image_paths: str, paths to the images
+        :param image_path: str, paths to the images
         :return: bytes, serialized embedding of faces from images
         """
         encodings = []
-
-        for image_path in image_paths:
-            image = cv2.imread(image_path)
-            rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            encodings.extend(face_recognition.face_encodings(rgb, face_recognition.face_locations(rgb, model='hog')))
+        image = cv2.imread(image_path)
+        rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        encodings.extend(face_recognition.face_encodings(rgb, face_recognition.face_locations(rgb, model='hog')))
         return pickle.dumps(encodings)
